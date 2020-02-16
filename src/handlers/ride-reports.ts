a2c16b2c6b7a13d7e58436604b2e555d9203fa0e
Brian Cantrell <bcantrell@gmail.com>
2020-02-16 07:23:26 -0500

    Don't exit early, configure scheduler correctly

--- src/handlers/ride-reports.ts
+++ src/handlers/ride-reports.ts
@@ -2,7 +2,5 @@ import { ScheduledHandler } from "aws-lambda"
 import { processReports } from "../airtable/ride-report"
 
 export const handle: ScheduledHandler = async (_event, _context) => {
-  console.log(_event)
-  processReports()
+  await processReports()
 }
-
