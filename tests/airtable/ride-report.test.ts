1055945b0ce93472a299d2b74f210079e4cf3b0c
Brian Cantrell <bcantrell@gmail.com>
2026-03-16 21:52:37 -0400

    update test id

--- tests/airtable/ride-report.test.ts
+++ tests/airtable/ride-report.test.ts
@@ -2,7 +2,7 @@ import { getRideReportById } from "../../src/airtable/ride-report"
 
 describe("getRideReportById", () => {
   test("it returns data of the right shape", async () => {
-    const testReportId = "recOiHxTTxFh8IHzl"
+    const testReportId = "rectI32qWqunhra5i"
     const rideReport = await getRideReportById(testReportId)
 
     expect(rideReport.Ride).toBe("West Hudson Dirt")
